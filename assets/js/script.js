bddd8fb9eb3daf383a39f0ae3b27652731de70ff
Michelle Suniga <suniga.mrs@gmail.com>
2018-11-20 15:15:27 +0800

    update | Day 2 PM

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	// alert("JS Linked");
 	$('.nav-toggle').click(function(){
 		$('.nav-toggle').toggleClass('nav-active'),
+		$('#myNav').toggleClass('d-none'),
 		$('.my-nav').toggleClass('my-nav-active');
 	})
-});
+});	
