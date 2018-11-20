4377b1fcf7be3f794c9499456b203afe45fe2814
Michelle Suniga <suniga.mrs@gmail.com>
2018-11-20 16:44:13 +0800

    update | Day 2 PM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,11 @@
 $(document).ready(function(){
 
 
-	function hide(){
-		$('body').removeClass('overflow-hidden');
-		$('.my-nav').removeClass('my-nav-active'),
-		$('.nav-toggle').removeClass('nav-active'),
-	}
+
 	// alert("JS Linked");
 	$('.nav-toggle').click(function(){
 		$('.nav-toggle').toggleClass('nav-active'),
 		$('.my-nav').toggleClass('my-nav-active'),
 		$('body').toggleClass('overflow-hidden');
-	})
-
-	$('.my-nav-active').click(function(){
-		$('.nav-toggle').toggleClass('nav-active'),
-		$('.my-nav').toggleClass('my-nav-active'),
-		$('body').toggleClass('overflow-hidden');
-	})
-
+	}),
 });	
