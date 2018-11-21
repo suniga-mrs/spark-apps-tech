7552c838923fb352ebb34f9fbb612ecc0cbb3425
Michelle Suniga <suniga.mrs@gmail.com>
2018-11-21 11:34:56 +0800

    updates | Day 3 AM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,38 @@
 $(document).ready(function(){
-
-
-
-	// alert("JS Linked");
 	$('.nav-toggle').click(function(){
 		$('.nav-toggle').toggleClass('nav-active'),
-		$('.my-nav').toggleClass('my-nav-active'),
-		$('body').toggleClass('overflow-hidden');
-	}),
+		$('.my-nav').toggleClass('my-nav-active');
+		// $('body').toggleClass('overflow-hidden');
+	})
+
+
+
 });	
+
+// $(document).ready(function(){
+	
+// 	$('.owl-carousel').owlCarousel({
+// 	    loop:true,
+// 	    margin:10,
+// 	    responsiveClass:true,
+// 	    responsive:{
+// 	        0:{
+// 	            items:1,
+// 	            nav:true
+// 	        },
+// 	        600:{
+// 	            items:3,
+// 	            nav:false
+// 	        },
+// 	        1000:{
+// 	            items:5,
+// 	            nav:true,
+// 	            loop:false
+// 	        }
+// 	    }
+// 	})
+
+
+
+// });	
+
